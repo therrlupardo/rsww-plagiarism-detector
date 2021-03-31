2224e16044323128bfb89d2e86ff384f57f47b8a
zemdom <misiaze@gmail.com>
2021-03-31 18:46:40 +0200

    [docker-compose] revert changes in client service

--- app/client/src/environments/environment.prod.ts
+++ app/client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://apigateway'
+  apiUrl: 'https://localhost:8080'
 };
--- app/client/src/environments/environment.ts
+++ app/client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://apigateway'
+  apiUrl: 'https://localhost:8080'
 };
