96052a749a891b5ae04527cb633b6bf105dc552a
JakubDaleki <daleki.jakub@gmail.com>
2021-06-13 20:43:37 +0200

    add try catch

--- app/spark_analysis_scripts/spark_plagiatarism_detection.py
+++ app/spark_analysis_scripts/spark_plagiatarism_detection.py
@@ -1,3 +1,4 @@
+import atexit
 import difflib
 import pyspark
 import os
@@ -5,9 +6,12 @@ import sys
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
+from pyspark.sql.types import StructField, StructType, StringType
 from collections import namedtuple
 
-from pyspark.sql.types import StructField, StructType, StringType
+
+source_id = sys.argv[1]
+analysis_repository_path = "hdfs://10.40.71.55:9000/group3/data.parquet"
 
 
 def precise_analysis(file):
@@ -22,56 +26,72 @@ def get_diff(file):
     return str(difflib.SequenceMatcher(None, source, file).get_matching_blocks())
 
 
-console_stdout = sys.stdout # backup current stdout
-sys.stdout = open(os.devnull, "w")
+def read_source():
+    # extracting file content
+    source_df = spark.read.parquet("hdfs://10.40.71.55:9000/group3/sources.parquet")
+    source_df = source_df.filter(source_df["FileId"] == source_id)
 
-spark_context = pyspark.SparkContext.getOrCreate(
-    pyspark.SparkConf() \
-        .setMaster("spark://10.40.71.55:7077") \
-        .setAppName("rsww3_analysis") \
-        .set("spark.executor.memory", "4096m") \
-        .set("spark.driver.port", os.environ.get("SPARK_DRIVER_PORT")) \
-        .set("spark.ui.port", os.environ.get("SPARK_UI_PORT")) \
-        .set("spark.blockManager.port", os.environ.get("SPARK_BLOCKMANAGER_PORT")) \
-        .set("spark.driver.host", "10.40.71.55") \
-        .set("spark.driver.bindAddress", "0.0.0.0")
-)
-spark = SparkSession.builder.config(conf=spark_context.getConf()).getOrCreate()
+    return source_df.collect()[0]["FileContent"]
 
-source_id = sys.argv[1]
-analysis_repository_path = "hdfs://10.40.71.55:9000/group3/data.parquet"
+
+def get_spark_session():
+    spark_context = pyspark.SparkContext.getOrCreate(
+        pyspark.SparkConf() \
+            .setMaster("spark://10.40.71.55:7077") \
+            .setAppName("rsww3_analysis") \
+            .set("spark.executor.memory", "4096m") \
+            .set("spark.driver.port", os.environ.get("SPARK_DRIVER_PORT")) \
+            .set("spark.ui.port", os.environ.get("SPARK_UI_PORT")) \
+            .set("spark.blockManager.port", os.environ.get("SPARK_BLOCKMANAGER_PORT")) \
+            .set("spark.driver.host", "10.40.71.55") \
+            .set("spark.driver.bindAddress", "0.0.0.0")
+    )
+    spark = SparkSession.builder.config(conf=spark_context.getConf()).getOrCreate()
+    
+    return spark
+
+
+def perform_analysis():
+    df = spark.read.schema(schema).parquet(analysis_repository_path)
+    df = df.withColumn("diff_quick", quick_analysis_udf(df["FileContent"]))
+    df = df.filter(df["diff_quick"] > 0.1)
+    df = df.cache()
+    df = df.withColumn("diff_exact", precise_analysis_udf(df["FileContent"]))
+    df = df.filter(df["diff_exact"] > 0.1)
+    df = df.withColumn("reversed", 1.0 / df["diff_exact"])
+    df = df.cache()
+    count = df.count()
+
+    result = df.groupBy().sum("reversed").collect()[0]
+
+    return count / float(result["sum(reversed)"])
+
 
 # defining udfs
 precise_analysis_udf = udf(precise_analysis)
 quick_analysis_udf = udf(quick_analysis)
 get_diff_udf = udf(get_diff)
 
-# extracting file content
-source_df = spark.read.parquet("hdfs://10.40.71.55:9000/group3/sources.parquet")
-source = source_df.filter(source_df["FileId"] == source_id).collect()[0]["FileContent"]
-
-# performing analysis
 schema = StructType([
     StructField("UserId", StringType(), True),
     StructField("FileId", StringType(), True),
     StructField("Repository", StringType(), True),
     StructField("FileName", StringType(), True),
     StructField("FileContent", StringType(), True)
-  ])
+])
 
-df = spark.read.schema(schema).parquet(analysis_repository_path)
-df = df.withColumn("diff_quick", quick_analysis_udf(df["FileContent"]))
-df = df.filter(df["diff_quick"] > 0.1)
-df = df.cache()
-df = df.withColumn("diff_exact", precise_analysis_udf(df["FileContent"]))
-df = df.filter(df["diff_exact"] > 0.1).sort("diff_exact", ascending=False)
-result = df.limit(25).collect()
+try:
+    spark = get_spark_session()
+except:
+    print("-1")
+    sys.exit()
 
-file_names = [row["FileName"] for row in result]
-results = [row["diff_exact"] for row in result]
-
-sys.stdout = console_stdout
-print(dict(zip(file_names, results)))
-
-spark.stop()
-spark_context.stop()
+try:
+    source = read_source()
+    result = perform_analysis()
+    print(result)
+except:
+    print("-1")
+finally:
+    spark.stop()
+    spark.sparkContext.stop()
--- app/spark_analysis_scripts/spark_save_source_file.py
+++ app/spark_analysis_scripts/spark_save_source_file.py
@@ -5,45 +5,60 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, StringType
 
-user_id = sys.argv[1]
-file_id = sys.argv[2]
-repository_name = sys.argv[3]
-file_name = sys.argv[4]
-file_content = sys.argv[5]
 
-spark_context = pyspark.SparkContext.getOrCreate(
-    pyspark.SparkConf() \
-        .setMaster("spark://10.40.71.55:7077") \
-        .setAppName("rsww3_save_source_file") \
-        .set( "spark.executor.memory", "4096m") \
-        .set("spark.driver.port", os.environ.get("SPARK_DRIVER_PORT")) \
-        .set("spark.ui.port", os.environ.get("SPARK_UI_PORT")) \
-        .set("spark.blockManager.port", os.environ.get("SPARK_BLOCKMANAGER_PORT")) \
-        .set("spark.driver.host", "10.40.71.55") \
-        .set("spark.driver.bindAddress", "0.0.0.0")
-)
-spark = SparkSession.builder.config(conf=spark_context.getConf()).getOrCreate()
+def get_spark_session():
+    spark_context = pyspark.SparkContext.getOrCreate(
+        pyspark.SparkConf() \
+            .setMaster("spark://10.40.71.55:7077") \
+            .setAppName("rsww3_analysis") \
+            .set("spark.executor.memory", "4096m") \
+            .set("spark.driver.port", os.environ.get("SPARK_DRIVER_PORT")) \
+            .set("spark.ui.port", os.environ.get("SPARK_UI_PORT")) \
+            .set("spark.blockManager.port", os.environ.get("SPARK_BLOCKMANAGER_PORT")) \
+            .set("spark.driver.host", "10.40.71.55") \
+            .set("spark.driver.bindAddress", "0.0.0.0")
+    )
+    spark = SparkSession.builder.config(conf=spark_context.getConf()).getOrCreate()
+    
+    return spark
 
-analysis_repository_path = "hdfs://10.40.71.55:9000/group3/data.parquet"
-tmp_path = "hdfs://10.40.71.55:9000/group3/tmp.parquet"
 
-schema = StructType([
-    StructField("UserId", StringType(), True),
-    StructField("FileId", StringType(), True),
-    StructField("Repository", StringType(), True),
-    StructField("FileName", StringType(), True),
-    StructField("FileContent", StringType(), True)
-])
+try:
+    spark = get_spark_session()
+except:
+    print("-1")
+    sys.exit()
 
-analysis_repository_df = spark.read.schema(schema).parquet(analysis_repository_path)
-analysis_repository_df.write.format("parquet").mode("overwrite").save(tmp_path)
-analysis_repository_df = spark.read.parquet(tmp_path)
+try:
+    user_id = sys.argv[1]
+    file_id = sys.argv[2]
+    repository_name = sys.argv[3]
+    file_name = sys.argv[4]
+    file_content = sys.argv[5]
 
-columns = ["UserId", "FileId", "Repository", "FileName", "FileContent"]
-row = (user_id, file_id, repository_name, file_name, file_content)
-source_file_df = spark.createDataFrame([row], columns)
-file_repository_df = analysis_repository_df.union(source_file_df)
-file_repository_df.write.format("parquet").mode("overwrite").save(analysis_repository_path)
+    analysis_repository_path = "hdfs://10.40.71.55:9000/group3/data.parquet"
+    tmp_path = "hdfs://10.40.71.55:9000/group3/tmp.parquet"
 
-spark.stop()
-spark_context.stop()
+    schema = StructType([
+        StructField("UserId", StringType(), True),
+        StructField("FileId", StringType(), True),
+        StructField("Repository", StringType(), True),
+        StructField("FileName", StringType(), True),
+        StructField("FileContent", StringType(), True)
+    ])
+
+    analysis_repository_df = spark.read.schema(schema).parquet(analysis_repository_path)
+    analysis_repository_df.write.format("parquet").mode("overwrite").save(tmp_path)
+    analysis_repository_df = spark.read.parquet(tmp_path)
+
+    columns = ["UserId", "FileId", "Repository", "FileName", "FileContent"]
+    row = (user_id, file_id, repository_name, file_name, file_content)
+    source_file_df = spark.createDataFrame([row], columns)
+    file_repository_df = analysis_repository_df.union(source_file_df)
+    file_repository_df.write.format("parquet").mode("overwrite").save(analysis_repository_path)
+    print("1")
+except:
+    print("-1")
+finally:
+    spark.stop()
+    spark.sparkContext.stop()
